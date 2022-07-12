7f566098f21647129f061bbe98d17158e463c5ae
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-07-11 19:17:47 -0600

    Add relational tests to models

--- spec/factories/invoice_items.rb
+++ spec/factories/invoice_items.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     item
     invoice 
     quantity { Faker::Number.between(1..52) }
-    unit_price { Faker::Number.between(2.5..500.0) }.round(2)
+    unit_price { (Faker::Number.between(2.5..500.0)).round(2) }
   end
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :item do
     name { Faker::Commerce.product_name }
     description { Faker::Coffee.notes }
-    unit_price { Faker::Number.between(2.5..500.0) }.round(2)
+    unit_price { (Faker::Number.between(2.5..500.0)).round(2) }
     merchant
   end
 end
