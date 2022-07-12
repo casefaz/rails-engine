748a5d7fd4de70b21170cc90cc740bf2fd7e941e
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-07-12 13:59:44 -0600

    Set up merchant_items controller

--- app/controllers/api/v1/merchant_items_controller.rb
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::MerchantItemsController < ApplicationController
   def index
-    binding.pry
+    merchant = Merchant.find(params[:merchant_id])
+    render json: MerchantSerializer.new(merchant.items)
   end
 end
--- spec/requests/api/v1/merchant_items_spec.rb
+++ spec/requests/api/v1/merchant_items_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe 'Get Merchant Items' do
     context 'happy path' do 
       it 'successfully produces the information' do 
         merchants = create_list(:merchant, 2)
+        items = create_list(:item, 5, merchant: merchants[0])
 
         get "/api/v1/merchants/#{merchants[0].id}/items"
 
