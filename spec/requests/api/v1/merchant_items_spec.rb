96c2b9c8ffd414a610a2c100fd01cb1080c6c9dd
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-07-12 12:23:44 -0600

    Early testing and file setup for merchant items

---
+++ spec/requests/api/v1/merchant_items_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'Get Merchant Items' do 
+  describe 'get the endpoints for the items belonging to a merchant' do 
+    context 'happy path' do 
+      it 'successfully produces the information' do 
+        merchants = create_list(:merchant, 2)
+
+        get "/api/v1/merchants/#{merchants[0].id}/items"
+
+        expect(response).to be_successful
+        expect(response).to have_http_status(200)
+      end 
+    end
+  end
+end
