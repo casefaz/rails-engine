c2973e1c258516a3d42888a650e6348cb900c224
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-07-12 15:34:59 -0600

    Add happy path testing for item index

--- spec/requests/api/v1/items/items_index_spec.rb
+++ spec/requests/api/v1/items/items_index_spec.rb
@@ -9,6 +9,21 @@ RSpec.describe 'Items Index' do
 
       expect(response).to be_successful
       expect(response).to have_http_status(200)
+
+      parsed_items = JSON.parse(response.body, symbolize_names: true)
+
+      expect(parsed_items[:data].count).to eq(5)
+      expect(parsed_items[:data][0][:attributes][:name]).to eq(items.first.name)
+      expect(parsed_items[:data][0].keys.length).to eq(3)
+      expect(parsed_items[:data][0][:attributes].keys.length).to eq(4)
+
+      parsed_items[:data].each do |item|
+        expect(item).to have_key(:id)
+        expect(item[:attributes]).to have_key(:name)
+        expect(item[:attributes]).to have_key(:description)
+        expect(item[:attributes]).to have_key(:unit_price)
+        expect(item[:attributes]).to have_key(:merchant_id)
+      end
     end 
   end
 end
