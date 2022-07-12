c3c554a4205620d32b7a427aa618a9c20142fe6a
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-07-12 14:49:26 -0600

    Postman tests for merchant items passing

--- spec/requests/api/v1/merchant_items_spec.rb
+++ spec/requests/api/v1/merchant_items_spec.rb
@@ -6,11 +6,14 @@ RSpec.describe 'Get Merchant Items' do
       it 'successfully produces the information' do 
         merchants = create_list(:merchant, 2)
         items = create_list(:item, 5, merchant: merchants[0])
-
+        # binding.pry
         get "/api/v1/merchants/#{merchants[0].id}/items"
 
         expect(response).to be_successful
         expect(response).to have_http_status(200)
+
+        items = JSON.parse(response.body, symbolize_names: true)
+        binding.pry
       end 
     end
   end
