12618ad30c0b791c2eecb8a702a7b5eec479a7a9
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-07-13 08:31:40 -0600

    Happy path for merchant item with all postman tests passing

--- spec/requests/api/v1/items/item_merchant_spec.rb
+++ spec/requests/api/v1/items/item_merchant_spec.rb
@@ -11,6 +11,12 @@ RSpec.describe 'Items Merchant' do
 
         expect(response).to be_successful
         expect(response).to have_http_status(200)
+
+        parsed_merchant = JSON.parse(response.body, symbolize_names: true)
+        # binding.pry
+        expect(parsed_merchant[:data].keys.count).to eq(3)
+        expect(parsed_merchant[:data][:attributes].count).to eq(1)
+        expect(parsed_merchant[:data][:attributes][:name]).to eq(merchant.name)
       end
     end
   end
