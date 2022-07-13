c4110db3e7bb395de28155ea315edabda9f3bb9e
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-07-13 07:54:43 -0600

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,9 @@ Rails.application.routes.draw do
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index], controller: 'merchant_items'
       end 
+      resources :items, only: [:index, :show, :create, :destroy, :update] do 
+        resources :merchants, only: [:show], controller: 'item_merchants'
+       end
     end
-  end
-
-  namespace :api do 
-    namespace :v1 do 
-       resources :items, only: [:index, :show, :create, :destroy, :update]
-    end 
   end
 end
