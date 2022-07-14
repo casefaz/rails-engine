285bf88e674062804efb103c570d8b015cf4e7d9
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-07-14 13:54:56 -0600

    Add dependent destroys to delete associated invoices

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@
 class Item < ApplicationRecord
   belongs_to :merchant
 
-  has_many :invoice_items
+  has_many :invoice_items, dependent: :destroy
   has_many :invoices, through: :invoice_items, dependent: :destroy
 
   def self.find_name(query)
