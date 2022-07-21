e809a0c4ff572fb9e82c7264ae600ff89196104e
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-07-21 13:43:07 -0600

    Get postman tests to pass

--- app/controllers/api/v1/revenue/merchants_controller.rb
+++ app/controllers/api/v1/revenue/merchants_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::Revenue::MerchantsController < ApplicationController
   def index 
-    
+    merchant = Merchant.top_merchants(params[:quantity])
+    render json: MerchantNameRevenueSerializer.new(merchant)
   end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -11,9 +11,9 @@ class Merchant < ApplicationRecord
   end
 
   def self.top_merchants(quantity)
-    .joins(invoices: [:invoice_items, :transactions])
+    joins(invoices: [:invoice_items, :transactions])
     .where(transactions: {result: 'success'}, invoices: {status: 'shipped'})
-    .select(:name, :id, 'SUM(invoice_items.quantity*invoice_items.unit_price) as revenue')
+    .select(:name, :id, 'SUM(invoice_items.quantity * invoice_items.unit_price) as revenue')
     .group(:id)
     .order(revenue: :desc)
     .limit(quantity)
